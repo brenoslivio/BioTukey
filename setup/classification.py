c7977a4492c79043e7ebd0ab6d5ea5fe4c1e1d45
brenoslivio <brenoslivio@usp.br>
2023-06-12 06:08:43 -0300

    Final adjustments for AutoML.

--- setup/classification.py
+++ setup/classification.py
@@ -15,6 +15,78 @@ import plotly.graph_objects as go
 import xgboost as xgb
 import pandas as pd
 
+def automl():
+    tab1, tab2, tab3 = st.tabs(['Performance Scoring', 'Confusion Matrix', 'Feature Importance'])
+
+    with tab1:
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.markdown("**Cross-validation**")
+            df_cv = pd.read_csv("result_dna/training_kfold(10)_metrics.csv")
+
+            st.markdown("**Accuracy**")
+            st.markdown(f"Average cross-validation accuracy score: **{float(df_cv['ACC']):.4f} +- {float(df_cv['std_ACC']):.4f}**")
+
+            st.markdown("**F1-score**")
+            st.markdown(f"Average cross-validation weighted F1-score score: **{float(df_cv['F1_w']):.4f} +- {float(df_cv['std_F1_w']):.4f}**")
+
+        with col2:
+            with open("result_dna/trained_model.sav", "rb") as file:
+                st.download_button(
+                    label="Download trained model",
+                    data=file,
+                    file_name='result_dna/trained_model.sav',
+                    use_container_width=True
+                )
+
+    with tab2:
+        df = pd.read_csv('result_dna/training_confusion_matrix.csv')
+
+        # Extract labels and confusion matrix values
+        labels = df.columns[1:-1].tolist()
+        
+        values = df.iloc[0:-1, 1:-1].values.tolist()
+
+        fig = go.Figure(data=go.Heatmap(
+            z=values,
+            x=labels,
+            y=labels,
+            colorscale='Purples'
+        ))
+
+        fig.update_layout(
+            title='Confusion Matrix',
+            xaxis_title='Predicted label',
+            yaxis_title='True label'
+        )
+
+        st.markdown("**Cross-validation**")
+
+        st.plotly_chart(fig, use_container_width=True)
+
+    with tab3:
+        df = pd.read_csv('result_dna/feature_importance.csv', sep=' ', header=None)
+
+        features = df[2].str.extract(r'\((.*?)\)')[0][::-1]
+
+        score_importances = df[3].str.extract(r'\((.*?)\)')[0].values.astype(float)[::-1]
+
+        fig = go.Figure(data=go.Bar(
+            x=features,
+            y=score_importances,
+            marker=dict(color=score_importances, colorscale='purples'),
+            hovertemplate='Feature: %{x}<br>Importance: %{y}<extra></extra>'
+        ))
+        fig.update_layout(
+            title="Feature Importance using BioAutoML",
+            xaxis_title="Features",
+            yaxis_title="Importance",
+        )
+
+        st.plotly_chart(fig, use_container_width=True)
+
+
 def manual_model_selection(model_selection, test):
     le = preprocessing.LabelEncoder()
 
@@ -99,16 +171,16 @@ def manual_model_selection(model_selection, test):
             with metric_col2:
                 st.markdown("**Accuracy**")
                 scores = cross_val_score(pipeline, X_train, y_train, cv=10, scoring='accuracy', n_jobs=-1)
-                st.write("Average cross-validation accuracy score:", scores.mean(), "+-", scores.std())
+                st.markdown(f"Average cross-validation accuracy score: **{scores.mean():.4f} +- {scores.std():.4f}**")
                 st.markdown("**Precision**")
                 scores = cross_val_score(pipeline, X_train, y_train, cv=10, scoring='precision_weighted', n_jobs=-1)
-                st.write("Average cross-validation weighted precision score:", scores.mean(), "+-", scores.std())
+                st.markdown(f"Average cross-validation weighted precision score: **{scores.mean():.4f} +- {scores.std():.4f}**")
                 st.markdown("**Recall**")
                 scores = cross_val_score(pipeline, X_train, y_train, cv=10, scoring='recall_weighted', n_jobs=-1)
-                st.write("Average cross-validation weighted recall score:", scores.mean(), "+-", scores.std())
+                st.markdown(f"Average cross-validation weighted recall score: **{scores.mean():.4f} +- {scores.std():.4f}**")
                 st.markdown("**F1-score**")
                 scores = cross_val_score(pipeline, X_train, y_train, cv=10, scoring='f1_weighted', n_jobs=-1)
-                st.write("Average cross-validation weighted F1-score score:", scores.mean(), "+-", scores.std())
+                st.markdown(f"Average cross-validation weighted F1-score score: **{scores.mean():.4f} +- {scores.std():.4f}**")
 
             if test:
                 st.markdown("---")
@@ -116,16 +188,31 @@ def manual_model_selection(model_selection, test):
                     
                     st.markdown("**Test set**")
 
+                    test_col1, test_col2 = st.columns(2)
+
                     X_test = st.session_state['test'][0]
                     y_test = le.transform(st.session_state['test'][1])
 
                     pipeline.fit(X_train.values, y_train)
                     y_pred = pipeline.predict(X_test)
 
-                    report = classification_report(le.inverse_transform(y_test), le.inverse_transform(y_pred), output_dict=True)
-                    
-                    st.markdown("Test set metrics:")
-                    st.dataframe(pd.DataFrame(report).transpose(), use_container_width=True)
+                    with test_col1:
+                        
+                        report = classification_report(le.inverse_transform(y_test), le.inverse_transform(y_pred), output_dict=True)
+                        
+                        st.markdown("Test set metrics:")
+                        st.dataframe(pd.DataFrame(report).transpose(), use_container_width=True)
+                    with test_col2:
+                        nameseqs = st.session_state['test'][2]
+
+                        # Concatenate variables column-wise
+                        df_concat = pd.concat([nameseqs, pd.Series(le.inverse_transform(y_test)), pd.Series(le.inverse_transform(y_pred))], axis=1)
+
+                        # Rename the columns
+                        df_concat.columns = ['nameseq', 'true', 'predicted']
+
+                        st.dataframe(df_concat, use_container_width=True)
+
                 else:
                     st.warning("Submit your test sets.")
 
@@ -186,56 +273,58 @@ def manual_model_selection(model_selection, test):
                     st.warning("Submit your test sets.")
 
 def load(seq_type, option, study_example):
-    col1, col2 = st.columns(2)
-
+    
     tab1, tab2 = st.tabs(['Manual Model', 'AutoML'])
 
     if 'data' not in st.session_state:
         st.warning("Please select and submit descriptors to use for classification within the Feature Engineering module.")
     else:
         
-        with col1:
-            model_selection = st.selectbox("Select a model:", ['Random Forest', 'XGBoost'])
+        with tab1:
 
-        with col2:
-            evaluation_selection = st.selectbox("Select an evaluation method:", ['10-fold cross-validation', '10-fold cross-validation and test set'])
+            col1, col2 = st.columns(2)
 
-        if evaluation_selection == '10-fold cross-validation':
-            with tab1:
+            with col1:
+                model_selection = st.selectbox("Select a model:", ['Random Forest', 'XGBoost'])
+
+            with col2:
+                evaluation_selection = st.selectbox("Select an evaluation method:", ['10-fold cross-validation', '10-fold cross-validation and test set'])
+
+            if evaluation_selection == '10-fold cross-validation':
                 manual_model_selection(model_selection, False)
 
-        elif evaluation_selection == '10-fold cross-validation and test set':
-            with st.spinner('Loading...'):
-                
-                if option == "Example":
-                    st.success("Test set loaded from study example successfully.")
-                    files, _ = utils.processing.load_study(study_example, False)
-                    features = utils.feature_extraction(files, st.session_state['data'][4], seq_type, False)
-
-                    features.pop("nameseq")
-                    labels = features.pop("label")
-
-                    st.session_state['test'] = [features.values.astype(float), labels]
-                else:
-                    with st.form("test_set"):
-                        uploaded_files = st.file_uploader("Submit test set files:", accept_multiple_files=True)
-
-                        submitted = st.form_submit_button("Submit")
+            elif evaluation_selection == '10-fold cross-validation and test set':
+                with st.spinner('Loading...'):
                     
-                    if submitted and uploaded_files:
-                        files, _ = utils.processing.process_files(uploaded_files, seq_type, False)
+                    if option == "Example":
+                        st.success("Test set loaded from study example successfully.")
+                        files, _ = utils.processing.load_study(study_example, False)
                         features = utils.feature_extraction(files, st.session_state['data'][4], seq_type, False)
 
-                        features.pop("nameseq")
+                        nameseqs = features.pop("nameseq")
                         labels = features.pop("label")
 
-                        st.session_state['test'] = [features.values.astype(float), labels]
+                        st.session_state['test'] = [features.values.astype(float), labels, nameseqs]
+                    else:
+                        with st.form("test_set"):
+                            uploaded_files = st.file_uploader("Submit test set files:", accept_multiple_files=True)
 
-            if 'test' in st.session_state:
-                manual_model_selection(model_selection, True)
+                            submitted = st.form_submit_button("Submit")
+                        
+                        if submitted and uploaded_files:
+                            files, _ = utils.processing.process_files(uploaded_files, seq_type, False)
+                            features = utils.feature_extraction(files, st.session_state['data'][4], seq_type, False)
+
+                            features.pop("nameseq")
+                            labels = features.pop("label")
+
+                            st.session_state['test'] = [features.values.astype(float), labels]
+
+                if 'test' in st.session_state:
+                    manual_model_selection(model_selection, True)
 
         with tab2:
-            st.write("Under construction")  # Placeholder for AutoML functionality
+            automl()
 
 
 
